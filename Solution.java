352ffff61b2ddb65219380aed7af3829f06b84e8
twilson1401 <74617725+twilson1401@users.noreply.github.com>
2020-11-17 19:11:31 +0000

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -39,10 +39,31 @@ public class Solution {
         return "NO";
     }
 
-    public static void main(String[] args) {
-        System.out.println(kangaroo(0, 3, 5, 2));
-        System.out.println(kangaroo(0, 3, 4, 2));
+    private static final Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] x1V1X2V2 = scanner.nextLine().split(" ");
+
+        int x1 = Integer.parseInt(x1V1X2V2[0]);
+
+        int v1 = Integer.parseInt(x1V1X2V2[1]);
+
+        int x2 = Integer.parseInt(x1V1X2V2[2]);
+
+        int v2 = Integer.parseInt(x1V1X2V2[3]);
+
+        String result = kangaroo(x1, v1, x2, v2);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
     }
 }
+
+
 
